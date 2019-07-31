162d21306925138ff8979d9b3eafe9c969f241d2
Daniel Kraft <d@domob.eu>
2019-07-31 20:00:03 +0200

    Add MIT license and fix author information.
    Add the MIT license for the files, and set author contact to Autonomous
    Worlds Ltd (which is the author of these Python bindings, although not
    Neoscrypt itself).

--- neoscryptmodule.c
+++ neoscryptmodule.c
@@ -1,3 +1,7 @@
+// Copyright (C) 2019 The Xaya developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <Python.h>
 
 #include "neoscrypt.h"
--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2019 The Xaya developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 from distutils.core import setup, Extension
 
 neoscrypt_module = Extension('neoscrypt',
@@ -8,7 +14,7 @@ neoscrypt_module = Extension('neoscrypt',
 setup (name = 'neoscrypt',
        version = '1.0',
        description = 'Bindings for the NeoScrypt proof-of-work algorithm',
-       author = 'John Doering',
-       author_email = 'ghostlander@phoenixcoin.org',
-       url = 'https://github.com/ghostlander/NeoScrypt',
+       author = 'Autonomous Worlds Ltd',
+       author_email = 'info@autonomousworlds.com',
+       url = 'https://github.com/xaya/neoscrypt_python',
        ext_modules = [neoscrypt_module])

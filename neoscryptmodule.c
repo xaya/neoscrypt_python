0bed5ea0e84e2c5eb419be5845b55c92709dab21
Daniel Kraft <d@domob.eu>
2024-03-05 17:27:37 +0100

    Define PY_SSIZE_T_CLEAN.
    On Python 3.10 and up, we need to explicitly define PY_SSIZE_T_CLEAN
    to make the module work.
    
    See https://stackoverflow.com/questions/70705404/systemerror-py-ssize-t-clean-macro-must-be-defined-for-formats.

--- neoscryptmodule.c
+++ neoscryptmodule.c
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
 #include "neoscrypt.h"
